b8576ea49691a8fd87683d75e7f558d3a2631be5
shahroz <shahroz@gmail.com>
2024-03-23 16:04:31 +0500

    best seller section added and working on it

---
+++ src/components/BestSeller.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function BestSeller() {
+  return (
+    <div className="bg-black">
+      <div className="container">
+        <div>
+          <h2>
+            <span className=" text-white text-uppercase">Best Sellers</span>
+          </h2>
+        </div>
+        <div>
+            
+        </div>
+      </div>
+    </div>
+  );
+}
